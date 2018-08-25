d7b4f138ffd8fd93973f3cb1ac5dfa87ff8147fe
Callum Duffy <duffyc8@tcd.ie>
2018-08-25 20:12:08 +0100

    v1.0, working beta. small bugs with blank rules in files

--- app/src/main/java/ie/duffydev/vatican/HomeActivity.java
+++ app/src/main/java/ie/duffydev/vatican/HomeActivity.java
@@ -3,9 +3,15 @@ package ie.duffydev.vatican;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,7 +40,62 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private boolean loadRules(){
-        tempRuleSetup();
+        //tempRuleSetup();
+
+        for(int i =0; i<4; i++){
+            if(!readRules(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean readRules(int ruleType){
+        String path = "txt/";
+
+        //set rule type
+        switch(ruleType){
+            case 0:
+                path += "Decree.txt";
+                break;
+            case 1:
+                path += "Confession.txt";
+                break;
+            case 2:
+                path += "Judgement.txt";
+                break;
+            default:
+                path += "Commandment.txt";
+        }
+
+        String line = "";
+        try{
+            InputStream inputStream = getAssets().open(path);
+            Log.i("TEST","opened assets for " + path);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            while(null != (line = bufferedReader.readLine())){
+                switch(ruleType){
+                    case 0:
+                        decrees.add(line);
+                        break;
+                    case 1:
+                        confessions.add(line);
+                        break;
+                    case 2:
+                        judgements.add(line);
+                        break;
+                    default:
+                        commandments.add(line);
+                }
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
         return true;
     }
 
